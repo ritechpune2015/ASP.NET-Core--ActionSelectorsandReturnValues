5b91e74ee21fe34247ca6592438f0b05eb844001
RevolutionAdmin <achyut.kendre@gmail.com>
2025-08-04 21:06:17 +0530

    Add project files.

---
+++ ActionSelectorsandReturnValues/Controllers/ActionVerbsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActionSelectorsandReturnValues.Controllers
+{
+	public class ActionVerbsController
+	{
+
+		[HttpPost]
+		public string SayHello(string id)
+		{
+			return "Say Hello 1 Called!";
+		}
+
+		[HttpGet]
+		public string SayHello()
+		{
+			return "Say Hello 2 Called!";
+		} 
+	}
+}
---
+++ ActionSelectorsandReturnValues/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActionSelectorsandReturnValues.Controllers
+{
+	public class HomeController
+	{
+		public string Index()
+		{
+			return "Home Index Called!";
+		}
+
+		[ActionName("Cal")]
+		public string CalculateValueUsingBinaryClassificationGuassianAlgorithm()
+		{
+			return "Value Calculated!";
+		}
+
+		[NonAction]
+		public string NotForCall()
+		{
+			return "Not For Call";
+		}
+	}
+}
